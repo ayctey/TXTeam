f87babc6d6d4963630463e024753c726d6d5cf73
ayctey <ayctey@aycteydeMacBook-Pro.local>
2015-01-02 21:09:20 +0800

    Initial Commit

---
+++ TXTeam/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TXTeam
+//
+//  Created by ayctey on 15-1-2.
+//  Copyright (c) 2015年 ayctey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
